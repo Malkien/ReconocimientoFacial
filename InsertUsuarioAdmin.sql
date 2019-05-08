56233a4abd25faf5825638bbba4ccd67fa03effe
Malkien <kevin1cuenca@gmail.com>
2019-05-08 19:27:19 +0200

    Añadir script de insert usuario Admin

---
+++ InsertUsuarioAdmin.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO fichapersonal
+	VALUES('Kevin Isaac','Cuenca Sarrias','77190698f',640502773,'Aquí mismo',null,'5','kevin1cuenca@gmail.com');
+INSERT INTO usuario
+	values(5,'admin','admin','ADMINISTRAR','77190698f');
