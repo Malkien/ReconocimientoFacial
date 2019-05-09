b5a66763d3ae7b822fe672648ae112676c8be7ec
Malkien <kevin1cuenca@gmail.com>
2019-05-09 14:25:43 +0200

    insert usuario reconocimiento

--- InsertUsuarioAdmin.sql
+++ InsertUsuarioAdmin.sql
@@ -3,3 +3,8 @@ INSERT INTO fichapersonal
 	VALUES('Kevin Isaac','Cuenca Sarrias','77190698f',640502773,'Aquí mismo',null,'5','kevin1cuenca@gmail.com');
 INSERT INTO usuario
 	values(5,'admin','admin','ADMINISTRAR','77190698f');
+    
+INSERT INTO fichapersonal
+	VALUES('Andres','Garcia Alarcon','77428170g',640502723,'Aquí mismo',null,'3','andres@gmail.com');
+INSERT INTO usuario
+	values(5,'andres','andres','RECONOCER','77428170g');
