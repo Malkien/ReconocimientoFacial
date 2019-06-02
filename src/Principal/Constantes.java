1d8a4a8b7565e950f86fa29b7192f21b14b830a7
Malkien <kevin1cuenca@gmail.com>
2019-06-02 14:43:51 +0200

    constantes guardado

---
+++ src/Principal/Constantes.java
@@ -0,0 +1,9 @@
+package Principal;
+
+public class Constantes {
+	public final static String conexionCadena= "jdbc:mysql://85.214.120.213:3306/reconocimientofacial"
+			+ "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	public final static String usuarioConexion = "1dam";
+	public final static String passwordConexion = "1dam";
+	public final static boolean debugMode = true;
+}
